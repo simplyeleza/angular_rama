a935b14bdc686600da706d91e0f0c9872469dda3
Simply Tutorials <techdocpcu@gmail.com>
2018-07-22 14:31:47 +0900

    server side ordering and sorting

--- js/loop.js
+++ js/loop.js
@@ -82,10 +82,24 @@ $scope.contacts.loadMore();
 $scope.$watch('search',function(newVal,oldVal){
 
 
-console.log('newVal');
+if (angular.isDefined(newVal)) {
 
 
-})
+	$scope.contacts.doSearch(newVal);
+     }
+   })
+
+
+
+$scope.$watch('order',function(newVal,oldVal){
+
+
+if (angular.isDefined(newVal)) {
+
+
+	$scope.contacts.doOrder(newVal);
+     }
+   })
 
 
 
@@ -110,6 +124,21 @@ this.persons.push(person);
 'isLoading':false,
 'selectedPerson': null,
 'persons' : [],
+'search' :null,
+'doSearch':function(search){
+  self.hasMore =true;
+  self.page=1;
+  self.persons=[];
+  self.search=search;
+  self.loadContacts();
+},
+'doOrder':function(order){
+  self.hasMore =true;
+  self.page=1;
+  self.persons=[];
+  self.ordering=order;
+  self.loadContacts();
+},
 'loadContacts': function(){
 
      if(self.hasMore && !self.isLoading){
@@ -118,7 +147,9 @@ this.persons.push(person);
 
      var params ={
 
-     	'page':self.page
+     	'page':self.page,
+     	'search':self.search,
+     	'ordering':self.ordering
      };
 
      Contact.get(params,function (data){
