e82732ded6d38098c0bc964ebcc8965a42ea4ef2
Simply Tutorials <techdocpcu@gmail.com>
2018-08-15 14:32:17 +0900

    A shopping list application

---
+++ js/shopping.js
@@ -0,0 +1,31 @@
+
+
+
+
+var app =angular.module("myShoppingList",[]);
+
+app.controller("myCtrl",function($scope){
+
+	$scope.products =["Milk","Bread","Cheese"];
+
+	$scope.addItem =function(){
+        $scope.errorText ="";
+        if($scope.products.indexOf($scope.addMe) == -1){
+        	$scope.products.push($scope.addMe);
+        } else {
+        	$scope.errorText ="This item already exists in our Shopping List";
+        }
+		
+
+	}
+
+	$scope.removeItem =function (x){
+        $scope.errortext = ""; 
+		$scope.products.splice(x,1);
+
+	}
+
+});
+
+
+
