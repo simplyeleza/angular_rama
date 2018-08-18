8db4abda6310761bec73cbe793d606fa715c5dc0
simplyeleza <techdocpcu@gmail.com>
2018-08-19 01:17:57 +0900

    Icons and Theming

--- src/boot.ts
+++ src/boot.ts
@@ -2,7 +2,17 @@
 
 
 module ContactManagerApp {
-	angular.module('contactManagerApp',['ngMaterial'])
+	angular.module('contactManagerApp',['ngMaterial','ngMdIcons'])
 	.service('userService',UserService)
-	.controller('mainController', MainController);
+	.controller('mainController', MainController)
+	.config(($mdIconProvider:angular.material.IIconProvider,
+		     $mdThemingProvider:angular.material.IThemingProvider) => {
+
+         $mdIconProvider.icon('menu','./assets/svg/menu.svg',24);
+
+         $mdThemingProvider.theme('default')
+         .primaryPalette('blue')
+         .accentPalette('red');
+
+	});
 }
